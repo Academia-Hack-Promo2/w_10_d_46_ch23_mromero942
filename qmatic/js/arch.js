2877119cd227369b67e8a79a47137c68d770f1ba
maria <mariiromero17@gmail.com>
2015-04-27 06:00:10 -0430

    no lo habia subido

---
+++ qmatic/js/arch.js
@@ -0,0 +1,52 @@
+var operacion_taquilla = new Array();
+var ticket = 000;
+var tiempo;
+
+function ope_taquilla() {
+  operacion_taquilla.push(ticket);
+  ticket++;
+  console.log(operacion_taquilla);
+  document.getElementById("operacion").innerHTML = "su numero es:" + ticket
+}
+
+//3ra edad 
+
+var operacion_taquilla3 = new Array();
+var ticket = 400;
+var tiempo;
+
+function ope_taquilla3() {
+  operacion_taquilla3.push(ticket);
+  ticket++;
+  console.log(operacion_taquilla3);
+  document.getElementById("").innerHTML = "su numero es:" + ticket
+}
+
+//atencion al cliente
+
+var atencion_cliente = new Array();
+var ticket = 600;
+var tiempo;
+
+function a_cliente() {
+ atencion_cliente.push(ticket);
+  ticket++;
+  console.log(atencion_cliente);
+  document.getElementById("").innerHTML = "su numero es:" + ticket
+
+}
+
+//atencion cliente 3ra edad 
+
+var atencion_cliente3 = new Array();
+var ticket = 800;
+var tiempo;
+
+function a_cliente3() {
+ atencion_cliente.push(ticket);
+  ticket++;
+  console.log(atencion_cliente3);
+  document.getElementById("").innerHTML = "su numero es:" + ticket
+
+}
+
